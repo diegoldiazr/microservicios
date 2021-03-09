15e97a39cbcd7c96a5dd8b686483972a702a65f6
Diaz Rabadan, Diego <ddiaz@eulen.com>
2021-03-09 17:01:18 +0100

    Incorporar zuul

---
+++ zuul/src/test/java/com/ddr/docker/netflix/spring/microservices/zuul/ZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddr.docker.netflix.spring.microservices.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
