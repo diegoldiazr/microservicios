7f357d4228cf9cfe2ddd0057c3345cabf2c36b33
Diaz Rabadan, Diego <ddiaz@eulen.com>
2021-03-11 17:16:30 +0100

    adaptacion docker-localhost

--- pipeline.sh
+++ pipeline.sh
@@ -1,12 +1,18 @@
+#stop Container
+docker stop eureka-server
+docker stop zuul
+docker stop greeting-microservice
+docker stop greetings-telegram
+
 #delete old images from docker
-docker rmi eureka:0.0.1-SNAPSHOT
+docker rmi eureka-server:0.0.1-SNAPSHOT
 docker rmi zuul:0.0.1-SNAPSHOT
 docker rmi greeting-microservice:0.0.1-SNAPSHOT
 docker rmi greetings-telegram:0.0.1-SNAPSHOT
 
 cd eureka
 mvn clean install
-docker build --rm -t eureka:0.0.1-SNAPSHOT .
+docker build --rm -t eureka-server:0.0.1-SNAPSHOT .
 cd ..
 cd zuul
 mvn clean install
